e40259959f2dfc7a1f9d53cb211180e5c01d5be6
Marc-André Lanciault <marcan@impresscms.org>
2007-12-23 15:43:30 +0000

    Importing SmartPartner module
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/smartpartner/trunk@331 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ notification_update.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+* $Id$
+* Module: SmartPartner
+* Author: The SmartFactory <www.smartfactory.ca>
+* Licence: GNU
+*/
+
+include_once '../../mainfile.php';
+include XOOPS_ROOT_PATH . '/include/notification_update.php';
+
+?>
